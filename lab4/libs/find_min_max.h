069362569cf4bfa7895b6fb0c99434ffaf880e81
toptuk49 <chyka228tigra@yandex.ru>
2025-09-30 23:53:16 +0300

    refactor(lab4): improve architecture and code style
    - Rename makefile to use capital letter
    - Extract additional source files to static and dynamic libraries
    - Improve Makefile styling
    - Fix styling and linting issues

---
+++ lab4/libs/find_min_max.h
@@ -0,0 +1,9 @@
+#ifndef FIND_MIN_MAX_H
+#define FIND_MIN_MAX_H
+
+#include "utils.h"
+
+struct MinMax GetMinMax(int *array, unsigned long long begin,
+                        unsigned long long end);
+
+#endif
--- lab4/src/parallel_min_max/find_min_max.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIND_MIN_MAX_H
-#define FIND_MIN_MAX_H
-
-#include "utils.h"
-
-struct MinMax GetMinMax(int *array, unsigned long long begin, unsigned long long end);
-
-#endif
